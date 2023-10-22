13590b1ebaacc4a5490cc6d4a55b63d751fdacab
Italo Santana Marques <148703269+ItaloSantana2@users.noreply.github.com>
2023-10-22 17:26:06 -0300

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,10 +1,17 @@
 import time
+# Função de ordenação do Bubble Sort
 
 def OrdenaBolha(dados, desenharDados, timeTick):
+# Loop para percorrer a lista (len(dados) - 1) vezes  
   for _ in range(len(dados)-1):
+# Loop para comparar pares de elementos adjacentes    
     for j in range(len(dados)-1):
+# Se o elemento atual for maior que o próximo, troca      
       if dados[j] > dados[j+1]:
         dados[j], dados[j+1] = dados[j+1], dados[j]
+# Chama a função desenharDados para atualizar a interface, destacando os elementos comparados em verde        
         desenharDados(dados, ['green' if x == j or x == j+1 else 'yellow' for x in range(len(dados))])
+# Pausa a execução por um curto período para controlar a velocidade da visualização
         time.sleep(timeTick)
+ # Após a ordenação, desenha todos os elementos em verde para indicar a conclusão do processo        
   desenharDados(dados, ['green' for x in range(len(dados))])
