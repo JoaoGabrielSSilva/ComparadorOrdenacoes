06fc03ccbcbad079bbd69ab4e6f7f8b4e88d8361
JoaoGabrielSSilva <90883086+JoaoGabrielSSilva@users.noreply.github.com>
2023-10-23 15:27:49 -0300

    Create quickSort.py

---
+++ quickSort.py
@@ -0,0 +1,54 @@
+import time
+def particao(dados, cabeca, ponta, desenharDados, timeTick):
+  borda = cabeca
+  pivot = dados[ponta]
+
+  desenharDados(dados, pegarListaCores(len(dados), cabeca, ponta, borda, borda))
+  time.sleep(timeTick)
+  for j in range(cabeca, ponta):
+    if dados[j] < pivot:
+      desenharDados(dados, pegarListaCores(len(dados), cabeca, ponta, borda, j, True))
+      time.sleep(timeTick)
+      dados[borda], dados[j] = dados[j], dados[borda] 
+      borda += 1
+
+    desenharDados(dados, pegarListaCores(len(dados), cabeca, ponta, borda, j))
+    time.sleep(timeTick)
+
+  desenharDados(dados, pegarListaCores(len(dados), cabeca, ponta, borda, ponta, True))
+  time.sleep(timeTick)
+  #trocando o pivô pelo valor da borda  
+  dados[borda], dados[ponta] = dados[ponta], dados[borda]
+  return borda
+
+def quickSort(dados, cabeca, ponta, desenharDados, timeTick):
+  if cabeca < ponta:
+    indiceParticao = particao(dados, cabeca, ponta, desenharDados, timeTick)
+
+    #Partição da esquerda
+    quickSort(dados, cabeca, indiceParticao - 1, desenharDados, timeTick)
+
+    #Partição da direita
+    quickSort(dados, indiceParticao + 1, ponta, desenharDados, timeTick)
+
+def pegarListaCores(qtdDados, cabeca, ponta, borda, indiceAtual,  estaTrocando = False):
+  listaCores = []
+  for i in range(qtdDados):
+    #colorindo a base
+    if i >= cabeca and i <= ponta:
+      listaCores.append('gray')
+    else:
+      listaCores.append('green')
+
+    if i == ponta:
+      listaCores[i] = 'blue'
+    elif i == borda:
+      listaCores[i] = 'red'
+    elif i == indiceAtual:
+      listaCores[i] = 'yellow'
+
+    if estaTrocando:
+      if i == borda or i == indiceAtual:
+        listaCores[i] = 'green'
+
+  return listaCores
