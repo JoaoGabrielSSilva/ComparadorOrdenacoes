e21c74724dcab4de8571718d459a041207324aa1
JoaoGabrielSSilva <90883086+JoaoGabrielSSilva@users.noreply.github.com>
2023-11-04 16:29:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
-# Importação das bibliotecas necessárias
-from tkinter import *  # Importa a biblioteca para criar a interface gráfica
-from tkinter import ttk  # Importa a biblioteca ttk para widgets temáticos
-import random  # Importa a biblioteca random para gerar números aleatórios
+from tkinter import *
+from tkinter import ttk
+import random
 from bubbleSort import OrdenaBolha  # Importa uma função "OrdenaBolha" do outro arquivo (bubbleSort.py)
-from quickSort import OrdenaRapidoPrincipal  # Importa uma função "OrdenaRapidoPrincipal" do outro arquivo (quickSort.py)
-from mergeSort import OrdenaMescla  # Importa uma função "OrdenaMescla" do outro arquivo (mergeSort.py)
-from bogoSort import bogoSort  # Importa uma função "bogoSort" do outro arquivo (bogoSort.py)
-
-# Inicialização da janela principal
+from quickSort import OrdenaRapidoPrincipal
+from mergeSort import  OrdenaMescla
+from bogoSort import bogoSort
+# Inicializa a janela principal
 root = Tk()
 
 # Configurações da janela
-root.title('Visualização de Algoritmos de Ordenação')  # Define o título da janela
-root.maxsize(900, 600)  # Define o tamanho máximo da janela
-root.config(bg='black')  # Define a cor de fundo da janela como preto
+root.title('Visualização de Algoritmos de Ordenação')
+root.maxsize(1300, 800)
+root.config(bg='black')
 
 # Variáveis
-algoritmo_selecionado = StringVar()  # Variável para armazenar o algoritmo selecionado
-dados = []  # Armazena os dados a serem ordenados
-quantPassos = IntVar()  # Contador de passos
-quantPassos.set(0)  # Inicializa o contador com 0
-dadosSalvos = []  # Armazena uma cópia dos dados para reiniciar a ordenação
+algoritmo_selecionado = StringVar()
+dados = []
+quantPassos = IntVar()
+quantPassos.set(0)
+dadosSalvos = []
 
 # Função para desenhar os dados na interface
 def desenharDados(dados, listaCores):
-    canvas.delete("all")  # Limpa o canvas
+    canvas.delete("all")
     alturaCanvas = 380
     larguraCanvas = 600
     larguraBarras = larguraCanvas / (len(dados) + 1)
@@ -37,11 +35,9 @@ def desenharDados(dados, listaCores):
         y0 = alturaCanvas - altura * 340
         x1 = (i + 1) * larguraBarras + deslocamento
         y1 = alturaCanvas
-        # Cria um retângulo no canvas para representar cada elemento da lista
         canvas.create_rectangle(x0, y0, x1, y1, fill=listaCores[i])
-        # Adiciona o valor de cada elemento como texto no retângulo
         canvas.create_text(x0 + 2, y0, anchor=SW, text=str(dados[i]))
-    root.update_idletasks()  # Atualiza a interface
+    root.update_idletasks()
 
 # Função para gerar dados aleatórios
 def Gerar():
@@ -52,52 +48,45 @@ def Gerar():
     tamanho = int(entradaQuantidade.get())
 
     dados = []
-    # Gera um valor aleatório na quantidade especificada
+    #Gera um valor aleatório na quantidade especificada
     for _ in range(tamanho):
         dados.append(random.randrange(valMin, valMax + 1))
     
-    # Cada valor gerado é passado para a lista dadosSalvos para ter a possibilidade de reiniciar a ordenação
+    #Cada valor gerado é passado para a lista dadosSalvos para ter a possibilidade de reiniciar a ordenação
     for i in range(tamanho):
         dadosSalvos.append(dados[i])
     
     desenharDados(dados, ['yellow' for x in range(len(dados))])
 
-# Função para reiniciar para a lista de dados inicial sem ordenação
+#Função para reiniciar para a lista de dados inicial sem ordenação
 def ReiniciarOrdenacao():
     global dadosSalvos
     global dados
     quantPassos.set(0)
-    if not dados:
-        return
+    if not dados: return
     tamanho = int(entradaQuantidade.get())
     dados = []
     for _ in range(tamanho):
         dados.append(dadosSalvos[_])
 
     desenharDados(dados, ['yellow' for x in range(len(dados))])
-
+    
 # Função para iniciar o algoritmo de ordenação
 def IniciarAlgoritmo():
     global dados
-    if not dados:
-        return
+    if not dados: return
 
-    # Escolha do algoritmo de ordenação
+    #Escolha do algoritmo de ordenação
     if menuAlgoritmo.get() == 'Quick Sort':
-        # Chama a função de ordenação do Quick Sort e atualiza o contador de passos
         quantPassos.set(OrdenaRapidoPrincipal(dados, 0, len(dados) - 1, desenharDados, escalaVelocidade.get()))
         
     elif menuAlgoritmo.get() == 'Bubble Sort':
-        # Chama a função de ordenação do Bubble Sort e atualiza o contador de passos
         quantPassos.set(OrdenaBolha(dados, desenharDados, escalaVelocidade.get()))
     elif menuAlgoritmo.get() == 'Merge Sort':
-        # Chama a função de ordenação do Merge Sort e atualiza o contador de passos
         quantPassos.set(OrdenaMescla(dados, desenharDados, escalaVelocidade.get()))
     elif menuAlgoritmo.get() == 'Bogo Sort':
-        # Chama a função de ordenação do Bogo Sort e atualiza o contador de passos
         quantPassos.set(bogoSort(dados, desenharDados, escalaVelocidade.get()))
     desenharDados(dados, ['green' for x in range(len(dados))])
-
 # Criação da interface
 frameInterface = Frame(root, width=200, height=380, bg='grey')
 frameInterface.grid(row=0, column=0, padx=5, pady=5)
@@ -107,16 +96,17 @@ canvas.grid(row=0, column=1, padx=5, pady=5)
 
 # Parte da Interface do Usuário
 # Linha 0
+
 Label(frameInterface, text="Algoritmo: ", bg='grey').grid(row=0, column=0, padx=5, sticky=W)
-menuAlgoritmo = ttk.Combobox(frameInterface, textvariable=algoritmo_selecionado, values=['Bubble Sort', 'Quick Sort', 'Merge Sort', 'Bogo Sort'])
+menuAlgoritmo = ttk.Combobox(frameInterface, textvariable=algoritmo_selecionado, values=['Bubble Sort','Quick Sort', 'Merge Sort', 'Bogo Sort' ])
 menuAlgoritmo.grid(row=0, column=1, padx=5, pady=5)
 menuAlgoritmo.current(0)
 
-# Contador de Passos
+#Contador de Passos
 Label(frameInterface, text="Passos: ", bg='grey').grid(row=1, column=0, padx=5, sticky=W)
-Label(frameInterface, textvariable=quantPassos, bg='grey').grid(row=1, column=1, padx=5, sticky=W)
+Label(frameInterface, textvariable= quantPassos, bg='grey').grid(row=1, column=1, padx=5, sticky=W)
 
-# Botão para reiniciar a ordenação
+#Botão para reiniciar a ordenação
 Button(frameInterface, text="Reset", command=ReiniciarOrdenacao, bg='red').grid(row=2, column=0, padx=5, pady=5)
 
 # Campo de texto "Valor Mínimo"
@@ -127,18 +117,20 @@ entradaMinimo.grid(row=3, column=0, padx=5, pady=5)
 entradaMaximo = Scale(frameInterface, from_=10, to=100, digits=2, resolution=1, orient=HORIZONTAL, label="Valor Máximo")
 entradaMaximo.grid(row=3, column=1, padx=5, pady=5)
 
-# Velocidade
-escalaVelocidade = Scale(frameInterface, from_=0.1, to=2.0, length=200, digits=2, resolution=0.2, orient=VERTICAL, label="Velocidade(s)")
+#Velocidade
+escalaVelocidade = Scale(frameInterface, from_=0.1, to=2.0, length=200, digits=2, resolution=0.2, orient=VERTICAL, label='''Duração do Passo:(Em Segundos)''')
 escalaVelocidade.grid(row=4, column=0, padx=5, pady=5)
 
 # Campo de texto "Quantidade"
 entradaQuantidade = Scale(frameInterface, from_=25, to=3, digits=2, resolution=1, orient=VERTICAL, label="Quantidade")
 entradaQuantidade.grid(row=4, column=1, padx=5, pady=5)
 
-# Botão para gerar um conjunto de dados
+#Botão para gerar um conjunto de dados
 Button(frameInterface, text="Gerar", command=Gerar, bg='white').grid(row=5, column=0, padx=5, pady=5)
 
-# Botão para iniciar a ordenação
+#Botão para iniciar a ordenação
 Button(frameInterface, text="Iniciar", command=IniciarAlgoritmo, bg='green').grid(row=5, column=1, padx=5, pady=5)
+
+
 
 root.mainloop()
